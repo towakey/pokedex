f444972ac148e492189d6440d94f172673b22aec
towakey <kyosgame@gmail.com>
2021-06-29 00:45:49 +0900

    scriptは削除

--- main.py
+++
@@ -1,8 +0,0 @@
-## -*- coding: utf-8 -*-
-from pokedex.pokedex import Pokedex
-
-pokedex = Pokedex()
-pokedex.load(1, 7, False)
-
-print(pokedex.name)
-print(pokedex.description['ja']['ultrasun_kanji'])
