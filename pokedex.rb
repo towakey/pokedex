775d37fc041e69b5fdaec42ad4db97f173d33ce5
towakey <kyosgame@gmail.com>
2025-01-01 00:23:32 +0900

    クラス化

--- pokedex.rb
+++ pokedex.rb
@@ -1,22 +1,44 @@
 require 'sqlite3'
 require 'json'
 
-# SQLiteの文字列をエスケープする関数
-def escape_sql_string(str)
+class PokedexImporter
+  def initialize(db_path = 'pokedex.db', json_path = './pokedex/pokedex.json')
+    @db_path = db_path
+    @json_path = json_path
+    @sqlite = SQLite3::Database.open(@db_path)
+  end
+
+  def import
+    @sqlite.transaction
+    begin
+      create_tables
+      import_data
+      @sqlite.commit
+    rescue SQLite3::Exception => e
+      @sqlite.rollback
+      raise e
+    end
+  end
+
+  private
+
+  def escape_sql_string(str)
     return '' if str.nil?
     str.to_s.gsub("'", "''")
-end
+  end
 
-sqlite = SQLite3::Database.open 'pokedex.db'
+  def create_tables
+    create_pokedex_table
+    create_version_table
+  end
 
-# Start transaction for table creation
-sqlite.transaction
-sql = <<SQL
+  def create_pokedex_table
+    sql = <<SQL
 DROP TABLE IF EXISTS pokedex;
 SQL
-sqlite.execute(sql)
+    @sqlite.execute(sql)
 
-sql = <<SQL
+    sql = <<SQL
 CREATE TABLE pokedex (
     id INTEGER,
     form TEXT,
@@ -33,36 +55,56 @@ CREATE TABLE pokedex (
     weight TEXT
 );
 SQL
-sqlite.execute(sql)
+    @sqlite.execute(sql)
+  end
 
-sql = <<SQL
+  def create_version_table
+    sql = <<SQL
 DROP TABLE IF EXISTS version;
 SQL
-sqlite.execute(sql)
+    @sqlite.execute(sql)
 
-sql = <<SQL
+    sql = <<SQL
 CREATE TABLE version (
     table_name TEXT,
     update_date TEXT,
     created_at TEXT
 );
 SQL
-sqlite.execute(sql)
+    @sqlite.execute(sql)
+  end
 
-pokedex = JSON.parse(File.read('./pokedex/pokedex.json'))
-sql = <<SQL
+  def import_data
+    pokedex_data = JSON.parse(File.read(@json_path))
+    insert_version_info(pokedex_data["update"])
+    import_pokemon_data(pokedex_data["pokedex"])
+  end
+
+  def insert_version_info(update_date)
+    sql = <<SQL
 INSERT INTO version (table_name, update_date, created_at)
 VALUES (
     '#{escape_sql_string('pokedex')}',
-    '#{escape_sql_string(pokedex["update"])}',
+    '#{escape_sql_string(update_date)}',
     '#{escape_sql_string(Time.now.to_s)}'
 );
 SQL
-sqlite.execute(sql)
+    @sqlite.execute(sql)
+  end
 
-pokedex["pokedex"].each do |pokemon|
+  def import_pokemon_data(pokemon_list)
+    pokemon_list.each do |pokemon|
+      import_base_form(pokemon)
+      import_region_forms(pokemon)
+      import_mega_evolutions(pokemon)
+      import_gigantamax_forms(pokemon)
+    end
+  end
+
+  def import_base_form(pokemon)
+    return unless pokemon[""]
     pokemon[""].each do |form|
-        sql = <<SQL
+      sql = <<SQL
 INSERT INTO pokedex
 VALUES (
     #{pokemon['id']},
@@ -80,13 +122,14 @@ VALUES (
     '#{escape_sql_string(form['weight'])}'
 );
 SQL
-        sqlite.execute(sql)
+      @sqlite.execute(sql)
     end
+  end
 
-    if pokemon["region_form"]
-        pokemon["region_form"].each do |form|
-            # puts form
-            sql = <<SQL
+  def import_region_forms(pokemon)
+    return unless pokemon["region_form"]
+    pokemon["region_form"].each do |form|
+      sql = <<SQL
 INSERT INTO pokedex
 VALUES (
     #{pokemon['id']},
@@ -104,13 +147,14 @@ VALUES (
     '#{escape_sql_string(form['weight'])}'
 );
 SQL
-            sqlite.execute(sql)
-        end
+      @sqlite.execute(sql)
     end
+  end
 
-    if pokemon["mega_evolution"]
-        pokemon["mega_evolution"].each do |form|
-            sql = <<SQL
+  def import_mega_evolutions(pokemon)
+    return unless pokemon["mega_evolution"]
+    pokemon["mega_evolution"].each do |form|
+      sql = <<SQL
 INSERT INTO pokedex
 VALUES (
     #{pokemon['id']},
@@ -128,14 +172,14 @@ VALUES (
     '#{escape_sql_string(form['weight'])}'
 );
 SQL
-            # puts sql
-            sqlite.execute(sql)
-        end
+      @sqlite.execute(sql)
     end
+  end
 
-    if pokemon["gigantamax"]
-        pokemon["gigantamax"].each do |form|
-            sql = <<SQL
+  def import_gigantamax_forms(pokemon)
+    return unless pokemon["gigantamax"]
+    pokemon["gigantamax"].each do |form|
+      sql = <<SQL
 INSERT INTO pokedex
 VALUES (
     #{pokemon['id']},
@@ -153,8 +197,13 @@ VALUES (
     '#{escape_sql_string(form['weight'])}'
 );
 SQL
-            sqlite.execute(sql)
-        end
+      @sqlite.execute(sql)
     end
+  end
 end
-sqlite.commit
+
+# スクリプトの実行
+if __FILE__ == $0
+  importer = PokedexImporter.new
+  importer.import
+end
