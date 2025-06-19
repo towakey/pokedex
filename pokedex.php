4c009bbf2328a2a23e68ab66e2ca123e47223fab
towakey <kyosgame@gmail.com>
2025-06-19 22:39:48 +0900

    CORS対応

--- pokedex.php
+++ pokedex.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 /**
  * SQLiteデータベース操作クラス
