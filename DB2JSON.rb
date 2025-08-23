bc21a500d49990df3a18e1efeb61e29cabad3b63
towakey <kyosgame@gmail.com>
2025-08-23 23:40:00 +0900

    対象を配列で設定するように変更

--- DB2JSON.rb
+++ DB2JSON.rb
@@ -1,9 +1,37 @@
 #!/usr/bin/env ruby
-# usage: ruby DB2JSON.rb X_Y
+# usage: ruby DB2JSON.rb
 
 require 'sqlite3'
 require 'json'
 require 'fileutils'
+
+# 処理対象のバージョンリスト
+# 形式: [メインバージョン, 参照元バージョン(省略可)]
+TARGET_VERSIONS = [
+  ['Red_Green_Blue_Pikachu'],
+  ['Gold_Silver_Crystal'],
+  ['Ruby_Sapphire_Emerald'],
+  ['Firered_Leafgreen'],
+  ['Diamond_Pearl_Platinum'],
+  ['Black_White'],
+  ['Black2_White2'],
+  ['X_Y'],
+  # ['OmegaRuby_AlphaSapphire'],
+  ['Sun_Moon'],
+  ['UltraSun_UltraMoon'],
+  # ['Let'sGoPikachu'],
+  # ['Let'sGoEevee'],
+  ['Sword_Shield'],
+  ['LegendsArceus'],
+  # ['BrilliantDiamond_ShiningPearl'],
+  ['Scarlet_Violet'],
+  # ['PokémonGo'],
+  # ['PokémonPinball'],
+  # ['PokémonRanger'],
+  # ['PokémonStadium'],
+  # ['PokémonStadium2'],
+  # ['New_Pokemon_Snap'],
+]
 
 # 引数(例: "LegendsArceus" / "Legends_Arceus" / "legends_arceus")を
 # データベース用のスネークケース(例: "legends_arceus")に正規化するメソッド
@@ -15,132 +43,132 @@ def normalize_version_name(name)
   name.gsub(/([a-z])([A-Z])/, '\\1\\2').downcase
 end
 
-RAW_VERSION = ARGV[0] || 'LegendsArceus'
-VERSION     = normalize_version_name(RAW_VERSION)
-puts VERSION
 DB_PATH = File.expand_path('pokedex.db', __dir__)
-OUT_DIR = File.join(__dir__, 'pokedex', VERSION)
-FileUtils.mkdir_p(OUT_DIR)
 
-# オプション: 第二引数にデータ取得元バージョンを指定出来る
-# 例) ruby DB2JSON.rb firered_leafgreen ruby_sapphire_emerald
-#     → 出力ファイルは firered_leafgreen.json だが、種族値などは
-#        ruby_sapphire_emerald から優先取得し、無い場合 firered_leafgreen を使う
-SOURCE_VERSION = (ARGV[1] || VERSION).downcase
-PRIORITY_VERSIONS = [SOURCE_VERSION, VERSION].uniq
-
-db = SQLite3::Database.new(DB_PATH)
-db.results_as_hash = true
-
-# 図鑑リストを取得
-pokedex_names = db.execute(<<~SQL, [VERSION]).map { |r| r['pokedex'] }.uniq
-  SELECT pokedex
-    FROM local_pokedex
-   WHERE version = ? COLLATE NOCASE
-SQL
-
-json_root = {
-  'update'       => Time.now.strftime('%Y%m%d'),
-  'game_version' => VERSION,
-  'pokedex'      => {}
-}
-
-# テーブルごとに参照元バージョンを上書きできる設定
-# 例: 'firered_leafgreen' で種族値などを 'ruby_sapphire_emerald' から取る
-FALLBACK_BY_DB = {
-  # type: {
-  #   'firered_leafgreen' => 'ruby_sapphire_emerald'
-  # },
-  # ability: {
-  #   'firered_leafgreen' => 'ruby_sapphire_emerald'
-  # },
-  # status: {
-  #   'firered_leafgreen' => 'ruby_sapphire_emerald'
-  # },
-  # description: {
-  #   'firered_leafgreen' => 'ruby_sapphire_emerald'
-  # }
-}
-
-# 汎用ヘルパー: 優先バージョン順で最初にヒットした行を返す
-def fetch_row_with_fallback(db, table_key, base_key, sql, specific_versions = nil)
-  versions = specific_versions || [FALLBACK_BY_DB.dig(table_key, VERSION), VERSION].compact.uniq
-  versions.each do |v|
-    row = db.get_first_row(sql, base_key + [v])
-    return row if row
-  end
-  nil
-end
-
-def generate_pokemon_id(no, region, spare1, spare2, gigantamax, mega_evolution, form, mf, out_of_index, shiny)
-  if region == 'アローラのすがた' then
-    region_value = '01'
-  elsif region == 'ガラルのすがた' then
-    region_value = '02'
-  elsif region == 'ヒスイのすがた' then
-    region_value = '03'
-  elsif region == 'パルデアのすがた' then
-    region_value = '04'
-  else
-    region_value = '00'
-  end
+# TARGET_VERSIONSの各バージョンを処理
+TARGET_VERSIONS.each do |version_config|
+  raw_version = version_config[0]
+  version = normalize_version_name(raw_version)
+  source_version = version_config[1] ? normalize_version_name(version_config[1]) : version
+  priority_versions = [source_version, version].uniq
   
-  if gigantamax.to_s.empty? then
-    gigantamax_value = '0'
-  else
-    gigantamax_value = '1'
-  end
+  puts "処理中: #{version}"
   
-  if mega_evolution.to_s.empty? then
-    mega_evolution_value = '0'
-  else
-    mega_evolution_value = '1'
-  end
+  out_dir = File.join(__dir__, 'pokedex', version)
+  FileUtils.mkdir_p(out_dir)
 
-  if form.to_s.empty?
-    form_value = '00'
-  elsif form.to_s =~ /^\d+$/
-    # If form is purely numeric (e.g., sequence index), use it directly with zero padding
-    form_value = form.to_i.to_s.rjust(2, '0')
-  else
-    form_value = '01'
-  end
+  db = SQLite3::Database.new(DB_PATH)
+  db.results_as_hash = true
 
-  # spare1 = '0'
-  # spare2 = '0'
-  # mf = '0'
-  # out_of_index = '000'
-  # shiny = '0'
-
-  no.to_s.rjust(4, '0') + "_" + region_value.to_s.rjust(2, '0') + spare1 + spare2 + gigantamax_value + mega_evolution_value + form_value + "_" + mf.to_s + "_" + out_of_index.to_s.rjust(3, '0') + "_" + shiny.to_s
-end
-
-pokedex_names.each do |pokedex_name|
-  # その図鑑に載っているポケモンを図鑑番号順に取得
-  pokemons = db.execute(<<~SQL, [VERSION, pokedex_name])
-    SELECT id, no, globalNo
+  # 図鑑リストを取得
+  pokedex_names = db.execute(<<~SQL, [version]).map { |r| r['pokedex'] }.uniq
+    SELECT pokedex
       FROM local_pokedex
-     WHERE version = ? COLLATE NOCASE AND pokedex = ?
-  GROUP BY no, globalNo
-  ORDER BY CAST(no AS INTEGER)
-SQL
+     WHERE version = ? COLLATE NOCASE
+  SQL
 
-  json_root['pokedex'][pokedex_name] =
-    pokemons.map do |row|
-      no        = row['no']
-      global_no = row['globalNo']
+  json_root = {
+    'update'       => Time.now.strftime('%Y%m%d'),
+    'game_version' => version,
+    'pokedex'      => {}
+  }
 
-      # 形態ごとの status をまとめる
-      forms = db.execute(<<~SQL, [VERSION, pokedex_name, global_no])
-        SELECT *
-          FROM local_pokedex
-         WHERE version = ? COLLATE NOCASE
-           AND pokedex = ?
-           AND globalNo = ?
-      SQL
-      # SQLite3::Database#execute が返す配列は凍結されている場合があるため、
-      # 非破壊版 uniq で新しい配列を受け取るように修正
-      forms = forms.uniq { |f| [f['form'], f['region'], f['mega_evolution'], f['gigantamax']] }
+  # テーブルごとに参照元バージョンを上書きできる設定
+  # 例: 'firered_leafgreen' で種族値などを 'ruby_sapphire_emerald' から取る
+  fallback_by_db = {
+    # type: {
+    #   'firered_leafgreen' => 'ruby_sapphire_emerald'
+    # },
+    # ability: {
+    #   'firered_leafgreen' => 'ruby_sapphire_emerald'
+    # },
+    # status: {
+    #   'firered_leafgreen' => 'ruby_sapphire_emerald'
+    # },
+    # description: {
+    #   'firered_leafgreen' => 'ruby_sapphire_emerald'
+    # }
+  }
+
+  # 汎用ヘルパー: 優先バージョン順で最初にヒットした行を返す
+  fetch_row_with_fallback = lambda do |db, table_key, base_key, sql, specific_versions = nil|
+    versions = specific_versions || [fallback_by_db.dig(table_key, version), version].compact.uniq
+    versions.each do |v|
+      row = db.get_first_row(sql, base_key + [v])
+      return row if row
+    end
+    nil
+  end
+
+  generate_pokemon_id = lambda do |no, region, spare1, spare2, gigantamax, mega_evolution, form, mf, out_of_index, shiny|
+    if region == 'アローラのすがた' then
+      region_value = '01'
+    elsif region == 'ガラルのすがた' then
+      region_value = '02'
+    elsif region == 'ヒスイのすがた' then
+      region_value = '03'
+    elsif region == 'パルデアのすがた' then
+      region_value = '04'
+    else
+      region_value = '00'
+    end
+    
+    if gigantamax.to_s.empty? then
+      gigantamax_value = '0'
+    else
+      gigantamax_value = '1'
+    end
+    
+    if mega_evolution.to_s.empty? then
+      mega_evolution_value = '0'
+    else
+      mega_evolution_value = '1'
+    end
+
+    if form.to_s.empty?
+      form_value = '00'
+    elsif form.to_s =~ /^\d+$/
+      # If form is purely numeric (e.g., sequence index), use it directly with zero padding
+      form_value = form.to_i.to_s.rjust(2, '0')
+    else
+      form_value = '01'
+    end
+
+    # spare1 = '0'
+    # spare2 = '0'
+    # mf = '0'
+    # out_of_index = '000'
+    # shiny = '0'
+
+    no.to_s.rjust(4, '0') + "_" + region_value.to_s.rjust(2, '0') + spare1 + spare2 + gigantamax_value + mega_evolution_value + form_value + "_" + mf.to_s + "_" + out_of_index.to_s.rjust(3, '0') + "_" + shiny.to_s
+  end
+
+  pokedex_names.each do |pokedex_name|
+    # その図鑑に載っているポケモンを図鑑番号順に取得
+    pokemons = db.execute(<<~SQL, [version, pokedex_name])
+      SELECT id, no, globalNo
+        FROM local_pokedex
+       WHERE version = ? COLLATE NOCASE AND pokedex = ?
+    GROUP BY no, globalNo
+    ORDER BY CAST(no AS INTEGER)
+    SQL
+
+    json_root['pokedex'][pokedex_name] =
+      pokemons.map do |row|
+        no        = row['no']
+        global_no = row['globalNo']
+
+        # 形態ごとの status をまとめる
+        forms = db.execute(<<~SQL, [version, pokedex_name, global_no])
+          SELECT *
+            FROM local_pokedex
+           WHERE version = ? COLLATE NOCASE
+             AND pokedex = ?
+             AND globalNo = ?
+        SQL
+        # SQLite3::Database#execute が返す配列は凍結されている場合があるため、
+        # 非破壊版 uniq で新しい配列を受け取るように修正
+        forms = forms.uniq { |f| [f['form'], f['region'], f['mega_evolution'], f['gigantamax']] }
 
       status_arr = forms.each_with_index.map do |f, idx|
         # フォームごとの連番 (01, 02, ...) を生成
@@ -149,7 +177,7 @@ SQL
         key = [f['globalNo'], f['form'], f['region'],
                f['mega_evolution'], f['gigantamax']]
 
-        type    = fetch_row_with_fallback(db, :type, key, <<~SQL)
+        type    = fetch_row_with_fallback.call(db, :type, key, <<~SQL)
              SELECT type1, type2
                FROM local_pokedex_type
               WHERE globalNo    = ?
@@ -160,7 +188,7 @@ SQL
                 AND version     = ? COLLATE NOCASE
            SQL
 
-        ability = fetch_row_with_fallback(db, :ability, key, <<~SQL)
+        ability = fetch_row_with_fallback.call(db, :ability, key, <<~SQL)
              SELECT ability1, ability2, dream_ability
                FROM local_pokedex_ability
               WHERE globalNo    = ?
@@ -171,7 +199,7 @@ SQL
                 AND version     = ? COLLATE NOCASE
            SQL
 
-        stats = fetch_row_with_fallback(db, :status, key, <<~SQL) || {}
+        stats = fetch_row_with_fallback.call(db, :status, key, <<~SQL) || {}
              SELECT hp, attack, defense, special_attack,
                     special_defense, speed
                FROM local_pokedex_status
@@ -184,10 +212,10 @@ SQL
            SQL
 
         # 対応するバージョン名を自動取得 (例: x_y → ["x","y"])
-        desc_versions = VERSION.split('_')
+        desc_versions = version.split('_')
         descriptions = {}
         desc_versions.each do |ver|
-          versions_list = [ver, FALLBACK_BY_DB.dig(:description, VERSION)].compact.uniq
+          versions_list = [ver, fallback_by_db.dig(:description, version)].compact.uniq
 
           sql = <<~SQL
             SELECT description
@@ -203,7 +231,7 @@ SQL
           SQL
 
           # base_key には version を追加し、SQL の version, ver 両方のプレースホルダに対応させる
-          desc_row = fetch_row_with_fallback(db, :description, key + [VERSION], sql, versions_list)
+          desc_row = fetch_row_with_fallback.call(db, :description, key + [version], sql, versions_list)
 
           descriptions[ver] = desc_row ? desc_row['description'] : ''
         end
@@ -229,12 +257,17 @@ SQL
         }
       end
 
-      # id = generate_pokemon_id(row['globalNo'], row['region'], '0', '0', row['gigantamax'], row['mega_evolution'], row['form'], '0', '000', '0')
-      id = row['id']
-      { 'id' => id, 'no' => no, 'globalNo' => global_no, 'status' => status_arr }
-    end
+        # id = generate_pokemon_id(row['globalNo'], row['region'], '0', '0', row['gigantamax'], row['mega_evolution'], row['form'], '0', '000', '0')
+        id = row['id']
+        { 'id' => id, 'no' => no, 'globalNo' => global_no, 'status' => status_arr }
+      end
+  end
+
+  File.write(File.join(out_dir, "#{version}.json"),
+             JSON.pretty_generate(json_root), mode: 'w:UTF-8')
+  puts "→ #{out_dir}/#{version}.json を生成しました"
+  
+  db.close
 end
 
-File.write(File.join(OUT_DIR, "#{VERSION}.json"),
-           JSON.pretty_generate(json_root), mode: 'w:UTF-8')
-puts "→ #{OUT_DIR}/#{VERSION}.json を生成しました"
+puts "全てのバージョンの処理が完了しました"
